b7c3de0e5b544f9e05d736d475747d57df9389c9
Philip Helger <philip@helger.com>
2017-08-25 12:40:33 +0200

    Using simpler API

--- src/main/java/com/helger/maven/buildinfo/JsonProps.java
+++ src/main/java/com/helger/maven/buildinfo/JsonProps.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2014-2017 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.helger.maven.buildinfo;
 
 import java.util.Map;
@@ -18,13 +34,7 @@ final class JsonProps extends JsonObject
   @Nonnull
   public JsonProps getChild (@Nonnull final String sName)
   {
-    IJson aChild = get (sName);
-    if (aChild == null)
-    {
-      aChild = new JsonProps ();
-      add (sName, aChild);
-    }
-    return (JsonProps) aChild;
+    return (JsonProps) computeIfAbsent (sName, k -> new JsonProps ());
   }
 
   @Nonnull
